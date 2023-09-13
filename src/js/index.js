cbdf5265dcc8c487c5418ccfca125944c8628bb0
NeLeNik <lnek10.1990@gmail.com>
2023-09-13 15:31:00 +0300

    footer and hero

--- src/js/index.js
+++ src/js/index.js
@@ -37,4 +37,43 @@ document.addEventListener('keydown', (e) => {
 	if (e.code === 'Escape') closeMenu();
 });
 
+// contacts
+class Spoiler {
+	static openedEl;
+
+	constructor(list) {
+		this.list = list;
+		this.btn = this.list.querySelector('.spoiler__trigger');
+		this.content = this.list.lastElementChild;
+		this.hideStr = `height: 0; visibility: hidden`;
+		this.showStr = `height: ${this.content.offsetHeight}px; visibility: visible`;
+		this.isOpened = false;
+
+		this.btn.addEventListener('click', () => {
+			if (Spoiler.openedEl && Spoiler.openedEl !== this)
+				Spoiler.openedEl.isOpened = false;
+			Spoiler.openedEl = this;
+			this.isOpened = !this.isOpened;
+		});
+	}
+
+	set isOpened(value) {
+		this._isOpened = value;
+		if (value) {
+			this.list.classList.add('opened');
+			this.content.style.cssText = this.showStr;
+		} else {
+			this.list.classList.remove('opened');
+			this.content.style.cssText = this.hideStr;
+		}
+	}
+
+	get isOpened() {
+		return this._isOpened;
+	}
+}
+
+const spoiler = document.querySelector('.spoiler');
+new Spoiler(spoiler);
+
 // animation
